77f7e8640df436e0dba593d77da1012c79d638e1
sentense1 <3xsaint@gmail.com>
2022-12-09 06:12:36 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		if ((c != 'q' && c != 'e') && c <= 'z')
+			putchar(c);
+	}
+		putchar('\n');
+
+	return (0);
+}
