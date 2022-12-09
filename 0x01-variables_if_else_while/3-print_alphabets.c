25060caf5b0f9b1e43d3a355aae3828ca1bba2b7
sentense1 <3xsaint@gmail.com>
2022-12-09 06:02:36 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	char j;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+
+	for (j = 'A' ; j <= 'Z' ; j++)
+	{
+		putchar(j);
+	}
+
+		putchar('\n');
+	return (0);
+}
