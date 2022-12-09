424520f6cd5a27fc324e4cb8cc716ae608221a6e
sentense1 <3xsaint@gmail.com>
2022-12-09 15:26:34 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j_d;
+	int k_d;
+
+	int i2;
+	int j_d2;
+	int k_d2;
+
+	while (i <= 98)
+	{
+		j_d = (i / 10 + '0');
+		k_d = (i % 10 + '0');
+		i2 = 0;
+
+		while (i2 <= 99)
+		{
+			j_d2 = (i2 / 10 + '0');
+			k_d2 = (i % 10 '0');
+
+			if (i < i2)
+			{
+				putchar(j_d);
+				putchar(k_d);
+				putchar(' ');
+				putchar(j_d2);
+				putchar(k_d2);
+
+				if (i != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			i2++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
