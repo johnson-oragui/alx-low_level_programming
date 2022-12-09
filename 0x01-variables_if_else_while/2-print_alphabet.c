eda3f193639e1862cbfe2d6bde0354d7cd55f646
sentense1 <3xsaint@gmail.com>
2022-12-09 05:43:12 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,15 @@
 
 int main(void)
 {
-	for (int x = 'A' ; x <= 'Z' ; x++)
+	char x;
+	
+	for (x = 'A' ; x <= 'Z' ; x++)
 	{
-		int lower_x = tolower(x);
 
-		putchar(lower_x);
+		putchar(x);
 	}
 
+	putchar("\n");
 
 	return (0);
 }
