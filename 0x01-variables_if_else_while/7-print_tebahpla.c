605f248673a2b4e7bd9ad5c2418ba9404cc4a814
sentense1 <3xsaint@gmail.com>
2022-12-09 10:51:01 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i 'z' ; i => 'a' ; i--)
+	{
+		putchar(i);
+	}
+		putchar('\n');
+	return (0);
+}
