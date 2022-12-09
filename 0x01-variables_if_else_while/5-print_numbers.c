0918692f069b722673b031894a77da05cda38234
sentense1 <3xsaint@gmail.com>
2022-12-09 10:31:59 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		printf("%d \n", i);
+	}
+	return (0);
+}
