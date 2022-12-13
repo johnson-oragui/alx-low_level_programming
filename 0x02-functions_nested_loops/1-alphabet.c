d64d5f8937a531a1de5e35cb8ffdd1ef07d98ee9
sentense1 <3xsaint@gmail.com>
2022-12-13 12:54:03 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * Main - Void print_alphabet
+ *
+ * Description - Prints alphabet in lower case
+ *
+ * Return: No return
+ */
+
+/*prints alphabet in lower case*/
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+		_putchar('\n');
+}
