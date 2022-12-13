af5e51f04b852898f9cea2756ba8d42bd79bf6bd
sentense1 <3xsaint@gmail.com>
2022-12-13 15:50:43 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
  */
 void times_table(void)
 {
-	int num, mult, product;
+	int num, multi, product;
 
 	for (num = 0; num <= 9; num++)
 	{
@@ -22,7 +22,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((product / 10) + '0');
-			_putchar((prroduct % 10) + '0');
+			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
 	}
