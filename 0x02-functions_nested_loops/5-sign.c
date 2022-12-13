f09c882c5dfc09718e65d327090399fa7db0770e
sentense1 <3xsaint@gmail.com>
2022-12-13 14:34:56 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('_');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
@@ -21,7 +21,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		_putchar('_');
+		_putchar('-');
 		return (-1);
 	}
 }
