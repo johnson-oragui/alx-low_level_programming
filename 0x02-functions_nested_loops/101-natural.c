c6850580374504f9bbc560d5bc8bdb6b450f97a5
sentense1 <3xsaint@gmail.com>
2022-12-14 10:54:04 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i, j = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			j += i;
+	}
+	printf("%d\n", j);
+
+	return (0);
+}
