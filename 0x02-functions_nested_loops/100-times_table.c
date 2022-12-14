1db6f6453a97042d7434d975f717ee7ca44cc295
sentense1 <3xsaint@gmail.com>
2022-12-14 05:22:11 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,36 +5,41 @@
  *
  * @n: the number times table
  */
+
+
 void print_times_table(int n)
+
 {
-	int num, multi, product;
 
-	for (num = 0; num <= n; num++)
+	int num, multi, prod;
+
+	if (n >= 0 && n <= 15)
 	{
-		if (n > 15)
+		for (num = 0; num <= n; num++)
 		{
-			break;
-		}
-		_putchar('0');
-
-		for (multi = 0; multi <= n; multi++)
-		{
-			if (multi == 0)
-			{
-				continue;
-			}
 			_putchar(',');
 			_putchar(' ');
-			_putchar(' ');
 
-			product = num * multi;
+			prod = num * multi;
 
-			if (product <= n)
+			if (prod <= 99)
+			{
 				_putchar(' ');
-			else
-				_putchar((product / 10) + '0');
-			_putchar((product % 10) + '0');
+			}
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			if (prod >= 100)
+			{
+				_putchar((prod / 100) + '0');
+				_putchar((prod / 10) % 10 + '0');
+			}
+			else if (prod <= 99 && prod >= 10)
+			{
+				_putchar((prod / 10) + '0');
+			}
+			_putchar((prod % 10) + '0');
 		}
-		_putchar('\n');
+		_putcha('\n');
 	}
-}
