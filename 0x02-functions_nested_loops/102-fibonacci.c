5c408ba13aa4dd3a533500055923ed47d79ea7b4
sentense1 <3xsaint@gmail.com>
2022-12-14 13:38:03 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int num1 = 1, num2 = 2, num3, i;
+
+	printf("%d, %d, ", num1, num2);
+
+	for (i = 0; i <= 50; i++)
+	{
+		num3 = num1 + num2;
+		printf("%d, ", num3);
+		num1 = num2;
+		num2 = num3;
+	}
+
+	return (0);
+}
