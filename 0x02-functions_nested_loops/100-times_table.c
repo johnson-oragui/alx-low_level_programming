08353b2dbfdbe105e7becba403b5e52ecfb3e534
sentense1 <3xsaint@gmail.com>
2022-12-14 03:26:21 -0800

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_times_table - print_times_table
+ *
+ * @n: the number times table
+ */
+void print_times_table(int n)
+{
+	int num, multi, product;
+
+	for (num = 0; num <= n; num++)
+	{
+		if (n > 15)
+		{
+			break;
+		}
+		putchar('0');
+
+		for (multi = 0; multi <= n; multi++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = num * multi;
+
+			if (product <= n)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0');
+			_putchar((product % 10) + '0');
+		}
+		_putchat('\n');
+	}
+}
