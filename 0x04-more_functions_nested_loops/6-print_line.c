0295beb47e631a08554b4c414d7d364534eaf85d
sentense1 <3xsaint@gmail.com>
2022-12-15 13:21:09 -0800

    void print_line(int n)

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ * Description: can only print with _putchar
+ * @n: number of times to print _
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(_);
+		if (n <= 0)
+			_putchar('\n');
+	}
+}
