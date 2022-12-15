8569d4708d99760f7dea890ec0161ac087102b10
sentense1 <3xsaint@gmail.com>
2022-12-15 02:53:05 -0800

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.lib>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: always 0 (success)
+ * On error, -1 is returned and error is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
