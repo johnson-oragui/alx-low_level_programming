aaa69945a5002e025add5e0e1d84718b800bc487
sentense1 <3xsaint@gmail.com>
2022-12-19 13:44:17 -0800

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _atoi - convert a string to an integer.
+ *
+ * @s: string to convert
+ *
+ * Return: 0 if there are no numbers in the string
+ */
+int _atoi(char *s)
+{
+	int to_num = atoi(s);
+
+	return (to_num);
+}
