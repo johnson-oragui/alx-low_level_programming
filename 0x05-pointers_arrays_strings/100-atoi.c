247d868ba5d48c765afc6848699ab7f1549d7279
sentense1 <3xsaint@gmail.com>
2022-12-20 16:44:15 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
 /**
  * _atoi - convert a string to an integer.
  *
@@ -9,34 +12,5 @@
 
 int _atoi(char *s)
 {
-
-	int i = 0;
-	int n = 0;
-	int signo = 1;
-
-	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
-	{
-		if (s[i] == '-')
-		{
-			signo += -1;
-		}
-
-		i++;
-	}
-
-	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
-	{
-		if (n >= 0)
-		{
-			n = n * 10 - (s[i] - '0');
-			i++;
-		}
-		else
-		{
-			n = n * 10 - (s[i] - '0');
-			i++;
-		}
-	}
-	signo *= -1;
-	return (n * signo);
+	return atoi(s);
 }
