a7dd6e492aa92232c5f6e964963e9104ff63b215
sentense1 <3xsaint@gmail.com>
2022-12-23 11:23:44 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,18 +12,9 @@ void reverse_array(int *a, int n)
 {
 	int i;
 
-	int temp;
-
-	for(i = 0; i<n/2; i++)
+	for(i = n - 1; i >= 0; i--)
 	{
-		temp = a[i];
-		a[i] = a[n-i-1];
-		a[n-i-1] = temp;
-	}
-
-	for(i = 0; i < n; i++)
-	{
-		if(i != 0)
+		if(i != n - 1)
 		{
 			printf(", ");
 		}
