45c71b4828eb766ca45a39ee3a614cb58997b3d6
sentense1 <3xsaint@gmail.com>
2022-12-23 14:19:33 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,7 +14,10 @@ char *rot13(char *s)
 	{
 		while ((s[i] > 65 && s[i] < 9) || (s[i] > 96 && s[i] < 123))
 		{
-			s[i] = (s[i] - 65 > 25) ? upper[s[i] - 97] : lower[s[i] - 65];
+			if (s[i] - 65 > 25)
+				s[i] = upper[s[i] - 97];
+			else
+				s[i] = lower[s[i] - 65];
 			i++;
 		}
 	}
