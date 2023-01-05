57a2bd3d2251ef1094d531c8e93b6008d0eea525
sentense1 <3xsaint@gmail.com>
2023-01-04 18:24:58 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - prints a string length
+ * @s: string to count
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
