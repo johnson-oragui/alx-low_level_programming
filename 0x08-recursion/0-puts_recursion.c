33c937a28f6451badb446c8192abe77e08e7860e
sentense1 <3xsaint@gmail.com>
2023-01-04 18:02:20 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_puts_recursion(s - 1);
+		printf("%s", s);
+	}
+}
