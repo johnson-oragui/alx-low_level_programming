ce214119628af6c7fb3f4a504b70c7ad00c57343
sentense1 <3xsaint@gmail.com>
2023-01-05 05:26:55 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,43 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * back_up_sqrt - support function for recursion
+ * @n: number to find the sqrt
+ * @vsqrt: natural sqrt
+ * Return: value of sqrt
+ */
+int back_up_sqrt(int n, int vsqrt)
+{
+	if (vsqrt * vsqrt == n)
+	{
+		return (vsqrt);
+	}
+	else
+	{
+		if (vsqrt * vsqrt > n)
+		{
+			return (-1);
+		}
+		else
+		{
+			return (back_up_sqrt(n, vsqrt + 1));
+		}
+	}
+}
+
+/**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to check for square root
  * Return: value of n
  */
 int _sqrt_recursion(int n)
 {
-	if (n == 1 || n == -1)
+	if (n < 0)
 	{
-		return (-1);
+		return (1);
 	}
-
-	if (_sqrt_recursion(n) % _sqrt_recursion(n - 1) == _sqrt_recursion(n - 1))
+	else
 	{
-		return _sqrt_recursion(n - 1);
+		return (back_up_sqrt(n, 0));
 	}
 }
