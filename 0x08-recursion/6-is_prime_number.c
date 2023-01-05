e20fded95dd80b4d8b3fe42caf055d4f6a1efe10
sentense1 <3xsaint@gmail.com>
2023-01-05 05:37:36 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,29 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * back_up_prime - returns prime number
+ * @n: integer prime
+ * @div: integer
+ * Return: prime value
+ */
+int back_up_prime(int n, int div)
+{
+	if (div < n)
+	{
+		if (n % div == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (back_up_prime(n, div + 2));
+		}
+	}
+	else
+	{
+		return (1);
+	}
+}
 /**
  * is_prime_number - prints prime number
  * @n: number to print
@@ -8,5 +31,12 @@
  */
 int is_prime_number(int n)
 {
-	if ()
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (back_up_prime(n, 2));
+	}
 }
