80c89967aba2ee62b70c5f2a94aa107600c74119
sentense1 <3xsaint@gmail.com>
2023-01-05 05:05:42 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s <= '\0')
+	if (*s == '\0')
 	{
+		printf("\n");
+		return;
+	}
+	if (*s != '\0')
+	{
+		printf(""%s), s;
 		_puts_recursion(s - 1);
-		printf("%s", s);
 	}
 }
