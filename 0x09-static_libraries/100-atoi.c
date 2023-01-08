da7d2d7675f6b0446af75782bf2398ea5a1c5ce5
sentense1 <3xsaint@gmail.com>
2023-01-08 13:16:02 -0800

    dd

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+#include <string.h>
+
+#include <limits.h>
+
+#include <stdlib.h>
+
+/**
+
+ * _atoi - convert a string to an integer.
+
+ *
+
+ * @s: string to be converted
+
+ *
+
+ * Return: value of _atoi
+
+ */
+
+
+
+int _atoi(char *s)
+
+{
+
+	return atoi(s);
+
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+
+ * _strlen - returns the length of a string
+
+ *
+
+ * @s: string to return its lenght
+
+ * Return: lenght of string
+
+ */
+
+int _strlen(char *s)
+
+{
+
+	int c;
+
+
+
+	for (c = 0; s[c] != '\0'; c++)
+
+		;
+
+
+
+	return (c);
+
+}
