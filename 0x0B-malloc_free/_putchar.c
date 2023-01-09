c9ce26a903daab64b56204083e64a6eb8a864b02
sentense1 <3xsaint@gmail.com>
2023-01-09 08:02:17 -0800

    malloc

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - prints a character
+ * @c: character to print
+ * Return: 0 if successful, 1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &7c, 1));
+}
