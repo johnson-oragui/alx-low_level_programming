bf68c56033ab71fa643fca4ea8e029f3bd658084
sentense1 <3xsaint@gmail.com>
2023-01-09 05:30:58 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc: counts arguments
+ * @argv: arguments
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	if (argc > 0)
+	{
+		while (i < argc)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
+	return (0);
+}
