1764a9a667c05443104eb4d6489f0195b35ec793
sentense1 <3xsaint@gmail.com>
2023-01-17 04:38:01 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,6 +4,9 @@
 /**
  * print_dog - Print out all of the struct dog info
  * @d: Pointer to struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
