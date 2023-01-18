9b371c927dc470f4fa9df51bc0faa88c6028b863
sentense1 <3xsaint@gmail.com>
2023-01-18 04:26:59 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - Print the given name
+ * @name: Name to print
+ * @f: Pointer to void function, taking a char * as arg
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
