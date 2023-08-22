bb1d1494b3ecd4957296d76345f68b2a1f8806eb
sentense1 <3xsaint@gmail.com>
2023-08-22 03:17:51 -0700

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 gcc -shared -o myprintf.so -fPIC test.c
-export LD_PRELOAD=/alx-low_level_programming/0x18-dynamic_libraries/myprintf.so
+export LD_PRELOAD=./myprintf.so
+./gm 9 8 10 24 75 9
